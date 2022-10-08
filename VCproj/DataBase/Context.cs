dd840cff1f1c7516c2f264c16323a33fe874e800
ahmedthabet97 <ahmedaboaouf333@gmail.com>
2022-10-08 20:57:38 +0200

    database

---
+++ VCproj/DataBase/Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCproj.DataBase
+{
+    public class Context: DbContext
+    {
+        public Context() : base(@"Data source = DESKTOP-T2U4HGU; initial catalog = inventory; integrated security = true;")
+        {
+
+        }
+        DbSet<Item> items { get; set; }
+        DbSet<Category>categories { get; set; }
+        DbSet<Seller> sellers { get; set; }
+        DbSet<Customer> customers { get; set; }
+        DbSet<Supplier> suppliers { get; set; }
+
+
+    }
+}
