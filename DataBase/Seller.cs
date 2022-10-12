3d43a8138ad8457188d872db0588ecab2067a8c3
islam12111998 <islam.18333388@compit.aun.edu.eg>
2022-10-12 17:56:26 +0200

    eslam

---
+++ DataBase/Seller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    public class Seller
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Phone { get; set; }
+        public string Address { get; set; }
+        public List<Customer> Customers { get; set; }
+
+        public List<Item> Items { get; set; }
+        public List<Bill> Bills { get; set; }
+    }
+}
---
+++ View/AddItem.cs
@@ -0,0 +1,46 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace View
+{
+    public partial class AddItem : Form
+    {
+        ItemServices ItemServices = new ItemServices();
+
+        public AddItem()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int ID = int.Parse(textBox1.Text);
+            //String Name = textBox2.Text;
+            //int CategoryId = int.Parse(textBox3.Text);
+            //int BuyPrice = int.Parse(textBox4.Text);
+            //int SellPrice = int.Parse(textBox5.Text);
+            //int Quantity = int.Parse(textBox6.Text);
+            //int SupplierId = int.Parse(textBox7.Text);
+
+            if (ItemServices.AddItem(1,"eslam", 500, 550, 4, 1, 1)>0)
+            {
+                MessageBox.Show("Add Success");
+
+            }
+            else
+            {
+                MessageBox.Show("Add Fail");
+
+            }
+        }
+    }
+}
